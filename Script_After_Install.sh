5c4cd637f65a89cbc7e0fab1aac2506493965407
Ashish <97246854+6abc@users.noreply.github.com>
2022-09-08 13:07:21 +0530

    Update Script_After_Install.sh

--- Script_After_Install.sh
+++ Script_After_Install.sh
@@ -2,12 +2,13 @@ sudo apt update
 sudo apt upgrade -y
 timedatectl set-timezone Asia/Kolkata
 sudo apt install curl -y
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 docker volume create portainer_data
 docker run -d -p 2000:8000 -p 2443:9443 --name portainer \
